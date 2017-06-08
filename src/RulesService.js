c4299ff8c06e1e1615840c4d9a7f3830720c3697
Mauricio Andrades <mod.and.op@gmail.com>
2017-06-07 17:10:30 -0700

    min

--- src/RulesService.js
+++ src/RulesService.js
@@ -1,17 +1,1 @@
-var LocalRulesService = function() {
-    this.get = function() {
-        if (localStorage["rules"]) {
-            return JSON.parse(localStorage["rules"])
-        } else {
-            return [];
-        }
-    };
-    this.set = function(data) {
-        localStorage["rules"] = JSON.stringify(data)
-    }
-};
-var RulesServiceFactory = {
-    getRulesService: function() {
-        return new LocalRulesService
-    }
-};
+var LocalRulesService=function(){this.get=function(){if(localStorage["rules"])return JSON.parse(localStorage["rules"]);else return[]};this.set=function(data){localStorage["rules"]=JSON.stringify(data)}};var RulesServiceFactory={getRulesService:function(){return new LocalRulesService}};
--- src/app.js
+++ src/app.js
@@ -1,2 +1,1 @@
-angular.module('switchmod', []).
-	factory('RulesService', RulesServiceFactory.getRulesService);
+angular.module("switchmod",[]).factory("RulesService",RulesServiceFactory.getRulesService);
