32589f0beb5b894a5bac2a890f30e79509e306bb
Mauricio Andrades <mod.and.op@gmail.com>
2017-08-26 20:52:02 -0700

    no min

--- src/RulesService.js
+++ src/RulesService.js
@@ -1,1 +1,14 @@
-var LocalRulesService=function(){this.get=function(){if(localStorage["rules"])return JSON.parse(localStorage["rules"]);else return[]};this.set=function(data){localStorage["rules"]=JSON.stringify(data)}};var RulesServiceFactory={getRulesService:function(){return new LocalRulesService}};
+var LocalRulesService = function() {
+    this.get = function() {
+        if (localStorage["rules"]) return JSON.parse(localStorage["rules"]);
+        else return []
+    };
+    this.set = function(data) {
+        localStorage["rules"] = JSON.stringify(data)
+    }
+};
+var RulesServiceFactory = {
+    getRulesService: function() {
+        return new LocalRulesService
+    }
+};
--- src/background.js
+++ src/background.js
@@ -1,1 +1,9 @@
-var ruleMatcher,rulesService,rules;rulesService=RulesServiceFactory.getRulesService();rules=rulesService.get();ruleMatcher=new RuleMatcher(rules);chrome.webRequest.onBeforeRequest.addListener(function(details){if(typeof details!=="undefined")return ruleMatcher.redirectOnMatch(details)},{urls:["<all_urls>"]},["blocking"]);
+var ruleMatcher, rulesService, rules;
+rulesService = RulesServiceFactory.getRulesService();
+rules = rulesService.get();
+ruleMatcher = new RuleMatcher(rules);
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+    if (typeof details !== "undefined") return ruleMatcher.redirectOnMatch(details)
+}, {
+    urls: ["<all_urls>"]
+}, ["blocking"]);
